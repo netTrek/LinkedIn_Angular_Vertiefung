147bd58ab7d7e8a715f223e69a801f02e7fd1968
netTrek <us@netTrek.de>
2018-02-27 18:02:49 +0100

    03_03_Element_mit_Direktive_erweitern

--- src/app/utils/hover.directive.ts
+++ src/app/utils/hover.directive.ts
@@ -1,12 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive ( {
   selector: '[inHover]'
 } )
 export class HoverDirective {
 
-  constructor () {
-    console.log ( 'inHover' );
+  constructor ( elem: ElementRef, renderer: Renderer2 ) {
+    console.log ( 'inHover' , elem );
+    // elem.nativeElement.style.color = 'red';
+    renderer.setStyle( elem.nativeElement , 'color', 'red');
   }
 
 }
