440fbd55a6e77521c7ac84c88d25c873555e1cdf
netTrek <us@netTrek.de>
2018-02-27 17:19:44 +0100

    03_01_Direktiven_erzeugen

---
+++ src/app/user/hover.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive ( {
+  selector: '[inHover]'
+})
+export class HoverDirective {
+  constructor () {
+    console.log ( 'dir hover' );
+  }
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserNameComponent } from './user-name/user-name.component';
+import { HoverDirective } from './hover.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserComponent, UserListComponent, UserNameComponent],
-  exports: [UserComponent]
+  declarations: [UserComponent, UserListComponent, UserNameComponent, HoverDirective],
+  exports: [UserComponent, HoverDirective]
 })
 export class UserModule { }
