19627c7e27535dc18e1b80e1e1dffa405cb97878
netTrek <us@netTrek.de>
2018-02-27 16:27:43 +0100

    02_09_ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'in-root',
+@Component ( {
+  selector   : 'in-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  styleUrls  : [ './app.component.scss' ]
+} )
 export class AppComponent {
-  title = 'in';
+
+  title        = 'in';
   showUserInfo = true;
+
+  class2bind = 'class-name-1 class-name-2 class-name-3';
 
 }
