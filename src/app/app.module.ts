053d64c0ff258e40011eb2307198f8ca1a32b48b
netTrek <us@netTrek.de>
2018-02-28 12:42:22 +0100

    05_03_Locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { PipeSamplesModule } from './pipe-samples/pipe-samples.module';
 
-@NgModule({
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData ( localeDe, 'de' );
+
+@NgModule ( {
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports     : [
     BrowserModule,
     UserModule,
     PipeSamplesModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  providers   : [
+    { provide: LOCALE_ID, useValue: 'de' }
+  ],
+  bootstrap   : [ AppComponent ]
+} )
+export class AppModule {
+}
