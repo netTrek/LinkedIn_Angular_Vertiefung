1f69b7bde36abe1238b87d2e95e6e335f63689e8
netTrek <us@netTrek.de>
2018-02-28 16:45:00 +0100

    09_03_Host_Self_und_Optional_Decorator

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,13 +1,17 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Host, Inject, OnInit, Optional, Self } from '@angular/core';
 import { AppModelService } from '../../app-model.service';
 import { LoginService } from '../login.service';
 import { IUseMe } from '../../iuse-me';
 import { USER_TOKEN } from '../../user-token';
+import { AppComponent } from '../../app.component';
 
 @Component ( {
   selector   : 'in-user',
   templateUrl: './user.component.html',
-  styleUrls  : [ './user.component.scss' ]
+  styleUrls  : [ './user.component.scss' ],
+  providers: [
+    {provide: USER_TOKEN, useValue: 'Danke!!!'}
+  ]
 } )
 export class UserComponent implements OnInit {
   constructor ( public appModel: AppModelService,
@@ -15,8 +19,11 @@ export class UserComponent implements OnInit {
                 @Inject ('useExist') useExist: any[],
                 @Inject ('mixed') mixed: any[],
                 @Inject ( USER_TOKEN ) user: string,
+                @Optional() @Host() @Inject ( USER_TOKEN ) userhost: string,
+                @Host() parent: AppComponent,
+                @Optional() @Self() @Inject ( USER_TOKEN ) userself: string,
                 login: LoginService ) {
-    console.log ( 'user', user);
+    console.log ( 'userself', userself);
   }
 
   ngOnInit () {
